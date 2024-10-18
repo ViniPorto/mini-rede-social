ee4360fa4e8f60bc7d2a401490409024380ca1ff
Vinícius Porto <leinateporto@gmail.com>
2024-10-17 22:56:37 -0300

    chore: commit inicial

---
+++ environments/environments.bat
@@ -0,0 +1,11 @@
+@echo off
+
+::Valores de desenvolvimento, não é de produção
+set MINI_REDE_SOCIAL_DB_HOST=localhost
+set MINI_REDE_SOCIAL_DB_PORT=1433
+set MINI_REDE_SOCIAL_DB_NAME=mini-rede-social
+set MINI_REDE_SOCIAL_DB_USER=sa
+set MINI_REDE_SOCIAL_DB_PASSWORD=1234578
+
+
+pause
---
+++ src/test/java/com/univille/mini_rede_social/MiniRedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univille.mini_rede_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
